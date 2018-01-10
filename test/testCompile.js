91223b76b5a12f0747338072daa343f706a7018f
Chun-Feng Liao <cfliao@nccu.edu.tw>
2018-01-10 15:12:46 +0800

    revise mocha test to TDD style

--- test/testCompile.js
+++ test/testCompile.js
@@ -1,18 +1,19 @@
 const ContractManager = require('../scm');
 const fs = require('fs');
 const chai = require('chai'), should = chai.should();
+const mocha = require('mocha'), suite = mocha.suite, test = mocha.test;
 
-describe('test the compile function of ContractManager', function () {
-    context('when the input is a string', function () {
+suite('ContractManager (compile)', () => {
+    context('when the input is a string', () => {
 
-        it('should provide error messages if there are errors', function () {
+        test('should provide error messages if there are errors', () => {
             ContractManager.compile('xxx', function (err, result) {
                 should.exist(err);
                 should.exist(result);
             });
         });
 
-        it('should not provide error if there is no error', function () {
+        test('should not provide error if there is no error', () => {
             ContractManager.compile(fs.readFileSync('./contracts/MyData.sol').toString(), function (err, result) {
                 should.not.exist(err);
                 should.exist(result);
@@ -20,8 +21,8 @@ describe('test the compile function of ContractManager', function () {
         });
     });
 
-    context('when the input is a file', function () {
-        it('should generate abi and bin and should not provide err if there is no error', function () {
+    context('when the input is a file', () => {
+        test('should generate abi and bin and should not provide err if there is no error', () => {
             ContractManager.compileFile('./contracts/MyData.sol', function (err, result) {
                 //TODO: not complete, and need to be revised. Need to generate the sol file before the test
                 should.not.exist(err);
